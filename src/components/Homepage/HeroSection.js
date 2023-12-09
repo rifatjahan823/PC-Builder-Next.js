9e8335b4389613f563664ca861590220d35f049a
rifatjahan823 <rifatjahan823@gmail.com>
2023-12-09 08:31:21 +0600

    create herosection slider

--- src/components/Homepage/HeroSection.js
+++ src/components/Homepage/HeroSection.js
@@ -5,32 +5,48 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
-import slide1 from '@/assets/slider-1.jpeg'
+import slide1 from "@/assets/slider-1.jpeg";
+import slide2 from "@/assets/slider-2.jpeg";
+import slide3 from "@/assets/slider-3.jpeg";
+import styles from '@/styles/Home.module.css'
 
 const HeroSection = () => {
   return (
-    <div className="w-full mx-auto px-5">
-      <Swiper
-        modules={[Pagination, Autoplay]}
-        autoplay={{
-          delay: 2000,
-        }}
-        loop={true}
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-        className="mySwiper"
-        centeredSlides={true}
-      >
-        <SwiperSlide>
-          {" "}
-          <Image
-            src={slide1}
-            alt="Picture of the author"
-          />
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-      </Swiper>
+    <div className="w-full mx-auto px-5 grid grid-cols-5 gap-4 mt-5">
+      {/* -----------Main Slider-------------- */}
+      <div className="col-span-3 bg-white shadow-md pb-8 relative">
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          autoplay={{
+            delay: 3000,
+          }}
+          loop={true}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          className={`${styles.mySwiper} ${styles.mySwiperPagination}`}
+          centeredSlides={true}
+        >
+          <SwiperSlide>
+            {" "}
+            <Image
+              src={slide1}
+              alt="Picture of the author"
+              className="w-full h-96"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image src={slide2} alt="Picture of the author"  className="w-full h-96"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Image src={slide3} alt="Picture of the author"  className="w-full h-96"/>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 };
